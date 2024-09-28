174a7a616e7674e86de72c5997d84e76f2f264cd
ZakuHuaso <102000700+ZakuHuaso@users.noreply.github.com>
2024-09-28 13:13:13 -0300

    Arreglos en tutorial,bienvenida y tabs

--- src/app/tutorial/tutorial.page.ts
+++ src/app/tutorial/tutorial.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tutorial',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onContinuar() {
+    this.router.navigate(['/home'])
   }
 
 }
